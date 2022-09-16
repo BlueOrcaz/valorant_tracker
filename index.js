9c559680e38d23790fad392e85ddcb1c7d1f47dc
compSci <bondoldman@gmail.com>
2022-09-16 11:25:40 +1000

    commandHandler

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const { GatewayIntentBits, Client } = require("discord.js");
+const fs = require('node:fs');
+const path = require('node:path');
+const { GatewayIntentBits, Client, Collection } = require("discord.js");
 const { token } = require('dotenv').config();
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+
 client.once('ready', () => {
-    console.log("Ready");
+    console.log("Ready!");
 });
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 
 client.login(process.env.DISCORD_TOKEN);
