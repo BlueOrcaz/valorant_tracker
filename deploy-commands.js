cf7e3fb8ce9b86da89286d6005e29a972c9e2a88
compSci <bondoldman@gmail.com>
2022-09-16 13:03:32 +1000

    updated deploy-commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, guildId, token } = require('dotenv').config();
+require('dotenv').config();
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
